16672fd9dde5568191d7522c2941574cc2203d39
JasonnnW056 <williamjason056@gmail.com>
2024-10-31 13:56:37 +1300

    Headers file

--- Class.cpp
+++ Class.cpp
@@ -6,7 +6,7 @@
 #include <sstream>
 #include <windows.h>
 #include <cstdlib> 
-
+#include "Display.h"
 #include "Class.h"
 
 using namespace std;
@@ -19,96 +19,175 @@ string User::getEmail() const {
     return email;
 }
 
+bool Student::getIsDomestic() const {
+    return isDomestic;
+}
 
-// Derived Student class
-//class Student : public User {
-//private:
-//    string firstName;
-//    string lastName;
-//    string course;
-//    bool isDomestic;
-//public:
-//    Student() = default;
-//    Student(string email, string password, string firstName, string lastName, string course, bool isDomestic)
-//        : User(email, password), firstName(firstName), lastName(lastName), course(course), isDomestic(isDomestic) {}
-//
-//    bool getIsDomestic() const {
-//        return isDomestic;
-//    }
-//
-//    void showDetails() const {
-//        cout << "Student Name: " << firstName << " " << lastName << endl;
-//        cout << "Email: " << email << endl;
-//        cout << "Course: " << course << endl;
-//        cout << "Status: " << (isDomestic ? "Domestic" : "International") << endl;
-//    }
-//
-//    // Save student details to a file
-//    void saveToFile(ofstream& outFile) const {
-//        outFile << email << " " << password << " " << firstName << " " << lastName << " "
-//            << course << " " << isDomestic << endl;
-//    }
-//
-//    // Load student details from a file
-//    static void loadFromFile(ifstream& inFile, unordered_map<string, Student>& students) {
-//        string email, password, firstName, lastName, course;
-//        bool isDomestic;
-//        while (inFile >> email >> password >> firstName >> lastName >> course >> isDomestic) {
-//            students[email] = Student(email, password, firstName, lastName, course, isDomestic);
-//        }
-//    }
-//};
+void Student::showDetails() const {
+    cout << "Student Name: " << firstName << " " << lastName << endl;
+    cout << "Email: " << email << endl;
+    cout << "Course: " << course << endl;
+    cout << "Status: " << (isDomestic ? "Domestic" : "International") << endl;
+}
+
+void Student::saveToFile(ofstream& outFile) const {
+    outFile << email << " " << password << " " << firstName << " " << lastName << " "
+        << course << " " << isDomestic << endl;
+}
+
+void Student::loadFromFile(ifstream& inFile, unordered_map<string, Student>& students) {
+    string email, password, firstName, lastName, course;
+    bool isDomestic;
+    while (inFile >> email >> password >> firstName >> lastName >> course >> isDomestic) {
+        students[email] = Student(email, password, firstName, lastName, course, isDomestic);
+    }
+}
+
+// Derived Admin class
+
+void Admin::viewAllStudents(const unordered_map<string, Student>& students) const {
+    cout << "\n-- All Students --" << endl;
+    for (const auto& pair : students) {
+        pair.second.showDetails();
+        cout << "-----------------" << endl;
+    }
+}
+
+void Admin::viewDomesticStudents(const unordered_map<string, Student>& students) const {
+    cout << "\n-- Domestic Students --" << endl;
+    for (const auto& pair : students) {
+        if (pair.second.getIsDomestic()) {
+            pair.second.showDetails();
+            cout << "-----------------" << endl;
+        }
+    }
+}
+
+void Admin::viewInternationalStudents(const unordered_map<string, Student>& students) const {
+    cout << "\n-- International Students --" << endl;
+    for (const auto& pair : students) {
+        if (!pair.second.getIsDomestic()) {
+            pair.second.showDetails();
+            cout << "-----------------" << endl;
+        }
+    }
+}
+
+void Admin::removeStudent(unordered_map<string, Student>& students, string studentEmail) const {
+    if (students.find(studentEmail) != students.end()) {
+        students.erase(studentEmail);
+        cout << "Student with email " << studentEmail << " has been removed." << endl;
+    }
+    else {
+        cout << "Student not found." << endl;
+    }
+}
+
+// Load admin details from a file
+void Admin::loadFromFile(ifstream& inFile, unordered_map<string, Admin>& admins) {
+    string email, password;
+    while (inFile >> email >> password) {
+        admins[email] = Admin(email, password);
+    }
+}
+
+// Function to hide password input
+string inputPassword() {
+    string password;
+    char ch;
+    cout << "Enter password: ";
+    while ((ch = _getch()) != '\r') { // '\r' is Enter key in Windows
+        if (ch == '\b' && !password.empty()) { // Handle backspace
+            password.pop_back();
+            cout << "\b \b";
+        }
+        else if (ch != '\b') {
+            password.push_back(ch);
+            cout << '*';
+        }
+    }
+    cout << endl;
+    return password;
+}
+
+// Function to sign up a student and save details to a file
+void studentSignUp() {
+    string email, password, firstName, lastName, course;
+    bool isDomestic;
+
+    cout << "Enter student details for sign-up:" << endl;
+    cout << "Email: ";
+    cin >> email;
+    password = inputPassword();
+    cout << "First Name: ";
+    cin >> firstName;
+    cout << "Last Name: ";
+    cin >> lastName;
+    cout << "Course: ";
+    cin >> course;
+    cout << "Is Domestic (1 for Yes, 0 for No): ";
+    cin >> isDomestic;
+
+    Student newStudent(email, password, firstName, lastName, course, isDomestic);
+    students[email] = newStudent;
+
+    ofstream outFile("students.txt", ios::app);
+    if (outFile.is_open()) {
+        newStudent.saveToFile(outFile);
+        outFile.close();
+        cout << "Student registered and saved successfully!" << endl;
+    }
+    else {
+        cout << "Error saving student to file." << endl;
+    }
+}
+
+// Function to load students from a file
+void loadStudentsFromFile() {
+    ifstream inFile("students.txt");
+    if (inFile.is_open()) {
+        Student::loadFromFile(inFile, students);
+        inFile.close();
+    }
+}
+
+// Function to load admins from a file
+void loadAdminsFromFile() {
+    ifstream inFile("admins.txt");
+    if (inFile.is_open()) {
+        Admin::loadFromFile(inFile, admins);
+        inFile.close();
+    }
+}
+
+// Sign-in function
+bool signIn(const string& email, const string& password) {
+    if (admins.find(email) != admins.end()) {
+        if (admins[email].login(email, password)) {
+            setColor(10);
+            cout << "Admin login successful!" << endl;
+            return true;
+        }
+        else {
+            setColor(12);
+            cout << "Incorrect password for admin." << endl;
+            return false;
+        }
+    }
+
+    if (students.find(email) != students.end()) {
+        if (students[email].login(email, password)) {
+            cout << "Student login successful!" << endl;
+            return true;
+        }
+        else {
+            cout << "Incorrect password for student." << endl;
+            return false;
+        }
+    }
+
+    cout << "Email not found." << endl;
+    return false;
+}
 
 
-//// Derived Admin class
-//class Admin : public User {
-//public:
-//    Admin() = default;
-//    Admin(string email, string password) : User(email, password) {}
-//
-//    void viewAllStudents(const unordered_map<string, Student>& students) const {
-//        cout << "\n-- All Students --" << endl;
-//        for (const auto& pair : students) {
-//            pair.second.showDetails();
-//            cout << "-----------------" << endl;
-//        }
-//    }
-//
-//    void viewDomesticStudents(const unordered_map<string, Student>& students) const {
-//        cout << "\n-- Domestic Students --" << endl;
-//        for (const auto& pair : students) {
-//            if (pair.second.getIsDomestic()) {
-//                pair.second.showDetails();
-//                cout << "-----------------" << endl;
-//            }
-//        }
-//    }
-//
-//    void viewInternationalStudents(const unordered_map<string, Student>& students) const {
-//        cout << "\n-- International Students --" << endl;
-//        for (const auto& pair : students) {
-//            if (!pair.second.getIsDomestic()) {
-//                pair.second.showDetails();
-//                cout << "-----------------" << endl;
-//            }
-//        }
-//    }
-//
-//    void removeStudent(unordered_map<string, Student>& students, string studentEmail) const {
-//        if (students.find(studentEmail) != students.end()) {
-//            students.erase(studentEmail);
-//            cout << "Student with email " << studentEmail << " has been removed." << endl;
-//        }
-//        else {
-//            cout << "Student not found." << endl;
-//        }
-//    }
-//
-//    // Load admin details from a file
-//    static void loadFromFile(ifstream& inFile, unordered_map<string, Admin>& admins) {
-//        string email, password;
-//        while (inFile >> email >> password) {
-//            admins[email] = Admin(email, password);
-//        }
-//    }
-//};
--- Class.h
+++ Class.h
@@ -8,8 +8,6 @@
 #include <windows.h>
 #include <cstdlib> 
 
-
-
 // Base User class
 class User {
 protected:
@@ -24,94 +22,64 @@ public:
     std::string getEmail() const;
 };
 
-// Derived Student class
-//class Student : public User {
-//private:
-//    string firstName;
-//    string lastName;
-//    string course;
-//    bool isDomestic;
-//public:
-//    Student() = default;
-//    Student(string email, string password, string firstName, string lastName, string course, bool isDomestic)
-//        : User(email, password), firstName(firstName), lastName(lastName), course(course), isDomestic(isDomestic) {}
-//
-//    bool getIsDomestic() const {
-//        return isDomestic;
-//    }
-//
-//    void showDetails() const {
-//        cout << "Student Name: " << firstName << " " << lastName << endl;
-//        cout << "Email: " << email << endl;
-//        cout << "Course: " << course << endl;
-//        cout << "Status: " << (isDomestic ? "Domestic" : "International") << endl;
-//    }
-//
-//    // Save student details to a file
-//    void saveToFile(ofstream& outFile) const {
-//        outFile << email << " " << password << " " << firstName << " " << lastName << " "
-//            << course << " " << isDomestic << endl;
-//    }
-//
-//    // Load student details from a file
-//    static void loadFromFile(ifstream& inFile, unordered_map<string, Student>& students) {
-//        string email, password, firstName, lastName, course;
-//        bool isDomestic;
-//        while (inFile >> email >> password >> firstName >> lastName >> course >> isDomestic) {
-//            students[email] = Student(email, password, firstName, lastName, course, isDomestic);
-//        }
-//    }
-//};
 
-//// Derived Admin class
-//class Admin : public User {
-//public:
-//    Admin() = default;
-//    Admin(string email, string password) : User(email, password) {}
-//
-//    void viewAllStudents(const unordered_map<string, Student>& students) const {
-//        cout << "\n-- All Students --" << endl;
-//        for (const auto& pair : students) {
-//            pair.second.showDetails();
-//            cout << "-----------------" << endl;
-//        }
-//    }
-//
-//    void viewDomesticStudents(const unordered_map<string, Student>& students) const {
-//        cout << "\n-- Domestic Students --" << endl;
-//        for (const auto& pair : students) {
-//            if (pair.second.getIsDomestic()) {
-//                pair.second.showDetails();
-//                cout << "-----------------" << endl;
-//            }
-//        }
-//    }
-//
-//    void viewInternationalStudents(const unordered_map<string, Student>& students) const {
-//        cout << "\n-- International Students --" << endl;
-//        for (const auto& pair : students) {
-//            if (!pair.second.getIsDomestic()) {
-//                pair.second.showDetails();
-//                cout << "-----------------" << endl;
-//            }
-//        }
-//    }
-//
-//    void removeStudent(unordered_map<string, Student>& students, string studentEmail) const {
-//        if (students.find(studentEmail) != students.end()) {
-//            students.erase(studentEmail);
-//            cout << "Student with email " << studentEmail << " has been removed." << endl;
-//        }
-//        else {
-//            cout << "Student not found." << endl;
-//        }
-//    }
-//
-//    // Load admin details from a file
-//    static void loadFromFile(ifstream& inFile, unordered_map<string, Admin>& admins) {
-//        string email, password;
-//        while (inFile >> email >> password) {
-//            admins[email] = Admin(email, password);
-//        }
-//    }
-//};
+// //Derived Student class
+class Student : public User {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string course;
+    bool isDomestic;
+public:
+    Student() = default;
+    Student(std::string email, std::string password, std::string firstName, std::string lastName, std::string course, bool isDomestic)
+        : User(email, password), firstName(firstName), lastName(lastName), course(course), isDomestic(isDomestic) {}
+
+    bool getIsDomestic() const;
+
+    void showDetails() const;
+
+    // Save student details to a file
+    void saveToFile(std::ofstream& outFile) const;
+
+    // Load student details from a file
+    static void loadFromFile(std::ifstream& inFile, std::unordered_map<std::string, Student>& students);
+
+};
+
+
+// Derived Admin class
+class Admin : public User {
+public:
+    Admin() = default;
+    Admin(std::string email, std::string password) : User(email, password) {}
+
+    void viewAllStudents(const std::unordered_map<std::string, Student>& students) const;
+
+    void viewDomesticStudents(const std::unordered_map<std::string, Student>& students) const;
+
+    void viewInternationalStudents(const std::unordered_map<std::string, Student>& students) const;
+
+    void removeStudent(std::unordered_map<std::string, Student>& students, std::string studentEmail) const;
+
+    // Load admin details from a file
+    static void loadFromFile(std::ifstream& inFile, std::unordered_map<std::string, Admin>& admins);
+};
+
+extern std::unordered_map<std::string, Student> students;
+extern std::unordered_map<std::string, Admin> admins;
+
+// Function to hide password input
+std::string inputPassword();
+
+// Function to sign up a student and save details to a file
+void studentSignUp();
+
+// Function to load students from a file
+void loadStudentsFromFile();
+
+// Function to load admins from a file
+void loadAdminsFromFile();
+
+// Sign-in function
+bool signIn(const std::string& email, const std::string& password);
