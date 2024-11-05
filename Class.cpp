82297346c4531ac38ddbacb5a16b36d2780720e4
JasonnnW056 <williamjason056@gmail.com>
2024-11-05 16:30:28 +1300

    json done!

--- Class.cpp
+++ Class.cpp
@@ -9,8 +9,10 @@
 #include "Display.h"
 #include "Class.h"
 #include <regex> // password validation libary added by (sami)
+#include "nlohmann/json.hpp"
 
 using namespace std;
+using namespace nlohmann;
 
 bool User::login(string inputEmail, string inputPassword) {
     return (email == inputEmail && password == inputPassword);
@@ -33,23 +35,43 @@ void Student::showDetails() const {
     cout << "Status: " << (isDomestic ? "Domestic" : "International") << endl;
 }
 
-void Student::saveToFile(ofstream& outFile) const {
-    outFile << email << " " << password << " " << firstName << " " << lastName << " "
-        << course << " " << isDomestic << endl;
-}
+//void Student::saveToFile(ofstream& outFile) const {
+//
+//    outFile << email << " " << password << " " << firstName << " " << lastName << " "
+//        << course << " " << isDomestic << endl;
+//}
 
-void Student::loadFromFile(ifstream& inFile, unordered_map<string, Student>& students) {
-    string email, password, firstName, lastName, course;
+//void Student::loadFromFile(ifstream& inFile, unordered_map<string, Student>& students) {
+//    string email, password, firstName, lastName, course;
+//    bool isDomestic;
+//    while (inFile >> email >> password >> firstName >> lastName >> course >> isDomestic) {
+//        students[email] = Student(email, password, firstName, lastName, course, isDomestic);
+//    }
+//}
+//json
+void Student::loadFromFileJSON(ifstream& inFile, unordered_map<string, Student>& students) {
+    string email, password, firstName, lastName, course, address;
+    int age;
     bool isDomestic;
-    while (inFile >> email >> password >> firstName >> lastName >> course >> isDomestic) {
-        students[email] = Student(email, password, firstName, lastName, course, isDomestic);
+
+    json inFiledata;
+    inFile >> inFiledata;
+
+    for (auto& i : inFiledata) {
+        email = i["email"];
+        password = i["password"];
+        firstName = i["firstName"];
+        lastName = i["lastName"];
+        course = i["course"];
+        address = i["address"];
+        age = i["age"];
+        isDomestic = i["isDomestic"];
+        students[email] = Student(email, password, firstName, lastName, course, address, age, isDomestic);
     }
+
 }
-
-
 
 // Derived Admin class
-
 void Admin::viewAllStudents(const unordered_map<string, Student>& students) const {
     cout << "\n-- All Students --" << endl;
     for (const auto& pair : students) {
@@ -88,15 +110,35 @@ void Admin::removeStudent(unordered_map<string, Student>& students, string stude
     }
 }
 
+//// Load admin details from a file 
+//void Admin::loadFromFile(ifstream& inFile, unordered_map<string, Admin>& admins) {
+//    string email, password;
+//    while (inFile >> email >> password) {
+//        admins[email] = Admin(email, password);
+//    }
+//}
 
-
-// Load admin details from a file
-void Admin::loadFromFile(ifstream& inFile, unordered_map<string, Admin>& admins) {
+// Load admin details from a file 
+//change to json
+void Admin::loadFromFileJSON(ifstream& inFile, unordered_map<string, Admin>& admins) {
     string email, password;
-    while (inFile >> email >> password) {
+
+    json inFiledata;
+    inFile >> inFiledata;
+    
+    for (auto& i : inFiledata) {
+        email = i["email"];
+        password = i["password"];
         admins[email] = Admin(email, password);
+      /*  cout << i["email"] << "  " << i["password"] << endl;*/
     }
+
+    /*while (inFile >> email >> password) {
+        admins[email] = Admin(email, password);
+    }*/
 }
+
+
 // function for password complexity
 bool validatePassword(const string& password) {
    // regex pattern for password validation
@@ -127,10 +169,78 @@ string inputPassword() {
     cout << endl;
     return password;
 }
+string inputAddress() {
+    string address{};
+    cout << "Address: ";
+    cin.clear();
+    cin.ignore();
+    getline(cin, address);
 
-// Function to sign up a student and save details to a file
-void studentSignUp() {
-    string email, password, firstName, lastName, course;
+    return address;
+}
+
+//// Function to sign up a student and save details to a file
+//void studentSignUp() {
+//    string email, password, firstName, lastName, course;
+//    bool isDomestic;
+//
+//    cout << "Enter student details for sign-up:" << endl;
+//    while (true) {
+//        cout << "Email: ";
+//        cin >> email;
+//        if (!validateEmail(email)) {
+//            cout << "Invalid email format. Please try again." << endl;
+//            continue;
+//        }
+//
+//        if (students.find(email) == students.end()) {
+//            break;
+//        }
+//        else {
+//            cout << "Email already exists. Please enter a new a different email." << endl;
+//        }
+//
+//    }
+//    while (true) {
+//        password = inputPassword();
+//        if (validatePassword(password)) {
+//            break;
+//        }
+//        else {
+//            cout << "Password is invalid. Please ensure it meets the following criteria:" << endl;
+//            cout << "- At least 8 characters long" << endl;
+//            cout << "- Contains at least one uppercase letter" << endl;
+//            cout << "- Contains at least one lowercase letter" << endl;
+//            cout << "- Contains at least one digit" << endl;
+//            cout << "- Contains at least one special character (e.g., @, #, $, etc.)" << endl;
+//        }
+//    }
+//        cout << "First Name: ";
+//        cin >> firstName;
+//        cout << "Last Name: ";
+//        cin >> lastName;
+//        cout << "Course: ";
+//        cin >> course;
+//        cout << "Is Domestic (1 for Yes, 0 for No): ";
+//        cin >> isDomestic;
+//
+//        Student newStudent(email, password, firstName, lastName, course, isDomestic);
+//        students[email] = newStudent;
+//
+//        ofstream outFile("students.txt", ios::app);
+//        if (outFile.is_open()) {
+//            newStudent.saveToFile(outFile);
+//            outFile.close();
+//            cout << "Student registered and saved successfully!" << endl;
+//        }
+//        else {
+//            cout << "Error saving student to file." << endl;
+//        }
+//    }
+// json
+void studentSignUpJSON() {
+    string email, password, firstName, lastName, course, address;
+    int age;
     bool isDomestic;
 
     cout << "Enter student details for sign-up:" << endl;
@@ -164,43 +274,72 @@ void studentSignUp() {
             cout << "- Contains at least one special character (e.g., @, #, $, etc.)" << endl;
         }
     }
-        cout << "First Name: ";
-        cin >> firstName;
-        cout << "Last Name: ";
-        cin >> lastName;
-        cout << "Course: ";
-        cin >> course;
-        cout << "Is Domestic (1 for Yes, 0 for No): ";
-        cin >> isDomestic;
+    cout << "First Name: ";
+    cin >> firstName;
+    cout << "Last Name: ";
+    cin >> lastName;
+    cout << "Age: ";
+    cin >> age;
 
-        Student newStudent(email, password, firstName, lastName, course, isDomestic);
-        students[email] = newStudent;
+    address = inputAddress();
 
-        ofstream outFile("students.txt", ios::app);
-        if (outFile.is_open()) {
-            newStudent.saveToFile(outFile);
-            outFile.close();
-            cout << "Student registered and saved successfully!" << endl;
-        }
-        else {
-            cout << "Error saving student to file." << endl;
-        }
+    cout << "Course: ";
+    cin >> course;
+    cout << "Is Domestic (1 for Yes, 0 for No): ";
+    cin >> isDomestic;
+
+    Student newStudent(email, password, firstName, lastName, course, address, age, isDomestic);
+    students[email] = newStudent;
+
+    ifstream inputFile("studentnew.json");
+    json existingdata;
+
+    if (inputFile.is_open()) {
+        inputFile >> existingdata;
+        inputFile.close();
     }
+
+
+    json studentdata{
+        {"email",email},
+        {"password",password},
+        {"firstName",firstName},
+        {"lastName",lastName},
+        {"course", course},
+        {"address",course},
+        {"age",age},
+        {"isDomestic", isDomestic},
+    };
+    existingdata.push_back(studentdata);
+
+    ofstream outFile("studentnew.json");
+    if (outFile.is_open()) {
+        
+        outFile << existingdata.dump(4);
+        outFile.close();
+        cout << "Student registered and saved successfully!" << endl;
+    }
+    else {
+        cout << "Error saving student to file." << endl;
+    }
+}
 
 // Function to load students from a file
 void loadStudentsFromFile() {
-    ifstream inFile("students.txt");
+    ifstream inFile("studentnew.json");
     if (inFile.is_open()) {
-        Student::loadFromFile(inFile, students);
+        /*Student::loadFromFile(inFile, students);*/
+        Student::loadFromFileJSON(inFile, students);
         inFile.close();
     }
 }
 
 // Function to load admins from a file
 void loadAdminsFromFile() {
-    ifstream inFile("admins.txt");
+    ifstream inFile("new_admins.json");
     if (inFile.is_open()) {
-        Admin::loadFromFile(inFile, admins);
+        /*Admin::loadFromFile(inFile, admins);*/
+        Admin::loadFromFileJSON(inFile, admins);
         inFile.close();
     }
 }
